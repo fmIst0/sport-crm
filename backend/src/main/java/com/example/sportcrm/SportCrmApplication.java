0c315e45f15addb76a0256ce90514f3cec07263d
mIst <bogdankharchenko77@gmail.com>
2024-01-10 19:58:21 +0200

    project created

---
+++ backend/src/main/java/com/example/sportcrm/SportCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportCrmApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/sportcrm/SportCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
